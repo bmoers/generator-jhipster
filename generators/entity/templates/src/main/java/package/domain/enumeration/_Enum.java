d85517b6e751271613085dc25663aec17fcc174e
Boris Moers <boris@moers.ch>
2016-08-11 13:29:59 +0200

    revert enum change

--- generators/entity/templates/src/main/java/package/domain/enumeration/_Enum.java
+++ generators/entity/templates/src/main/java/package/domain/enumeration/_Enum.java
@@ -4,12 +4,5 @@ package <%=packageName%>.domain.enumeration;
  * The <%= enumName %> enumeration.
  */
 public enum <%= enumName %> {
-    <%- enumValues %>;
-    <% if (enumValues.indexOf("(")!=-1){ %>
-    private Object value;
-	private <%= enumName %> (Object value){this.value = value;}
-    
-	@com.fasterxml.jackson.annotation.JsonValue
-    public String toString(){return String.valueOf(this.value);}
-    <% } %>
+    <%- enumValues %>
 }
